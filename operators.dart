aaa90e8d173be552f1688e5adef4947a7d71e7ea
Ankit Yadav <ankityadav997036@gmail.com>
2022-05-09 14:19:26 +0530

    1.Operators in dart 2.how to use it 3.Different type of Operators

---
+++ operators.dart
@@ -0,0 +1,80 @@
+
+void main(List<String> args) {
+  //Opetors
+  //arithmatic operater
+  var a = 2;
+  var b = 3;
+  var c = 4;
+  var d = 5;
+  print("Addition");
+  //Addition of numbers
+  print(a + b);
+  print("Subtraction");
+  //Subtraction of numbers
+  print(a - b);
+  print("Multyplication");
+// Multyplication of numbers
+  print(a * b * c);
+  print("Result is a double");
+  // Get Result is a double
+  print(a / b);
+  print("Result is a integer");
+  //get Result is a integer
+  print(a ~/ d);
+  print("Remainder");
+  // get Remainder
+  print(a % d);
+
+  //prefix  and postfix incriment and decriment Operators
+  print("Incriment");
+  //Incriments of values
+  var i = 4;
+  print(i);
+  print(i++);
+  print(i);
+  print("Decriment");
+  //Decriment of values
+  var j = 4;
+  print(j);
+  print(j--);
+  print(j);
+
+  /// equality and relational operators
+  print("equality operators");
+  //equality operators
+  print(a == b);
+  print('Not equal');
+  //Not equal
+  print(a != b);
+  print("Greater Then");
+  // Greater Then
+  print(a > b);
+  print("Shift right");
+  //Shift righ
+  print(a >> b);
+  print("Shift left");
+  //Shift left
+  print(a << b);
+  print("Less than");
+  //Less than
+  print(a < b);
+
+  /// Assignment operators
+  print("Less then Equals to");
+  //Less then Equals to
+  print(a <= b);
+  print("Greter then Equals to");
+  //Greter then Equals to
+  print(a >= b);
+  print("multyply and Equals to like (a=a*b)");
+  //multyply and Equals to like (a=a*b)
+  print(a *= b);
+  print("Divide and equals to (a=a/b");
+  //Divide and equals to (a=a/b)
+  print(a ~/= b);
+
+  print(a &= b);
+  print("Get remainder (a=a%b)");
+  //Get remainder (a=a%b)
+  print(a %= b);
+}
